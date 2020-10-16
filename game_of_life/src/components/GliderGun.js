0270219d18f4d688ea3e66ed42ec1b6657bbfcf4
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-10-15 19:11:03 -0700

    Initial Grid Setup, toggle cell

---
+++ game_of_life/src/components/GliderGun.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import produce from "immer"
+
+const numRows = 50;
+const numCols = 50;
+
+const GliderGun = props => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0))
+    }
+
+    return rows
+  });
+
+  // console.log(grid);
+  return (
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}>
+      {grid.map((rows, i) => rows.map((col, k) =>
+        <div
+          key={`${i}-${k}`}
+          // Update grid when clicked/toggle
+          onClick={() => {
+            const newGrid = produce(grid, gridCopy => {
+              gridCopy[i][k] = grid[i][k] ? 0 : 1;
+            })
+            setGrid(newGrid)
+          }}
+
+          style={{
+            width: 20,
+            height: 20,
+            backgroundColor: grid[i][k] ? 'pink' : undefined,
+            border: 'solid 1px black'
+          }} />)
+      )}
+    </div>
+  )
+}
+
+export default GliderGun;
